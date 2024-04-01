0ab0fa50baeb1047e01a8c1307d99758ac69d993
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-04-02 01:07:35 +0200

    Update globals.js

--- src/globals.js
+++ src/globals.js
@@ -1,2 +1,5 @@
-export let currentPage = 1;
+// Declara la variable currentPage como una variable global
+let currentPage = 1;
+
+// Exporta la variable currentPage para que pueda ser utilizada en otros archivos
 export { currentPage };
