0c6cb813c2ee0e162272f45faab3d5f4eec5e6bf
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-04-01 23:15:42 +0200

    Update button.js

--- src/button/button.js
+++ src/button/button.js
@@ -1,8 +1,17 @@
-import './button.css'
+import './button.css';
 
 export const createButton = () => {
-  const showMoreButton = document.createElement('button')
-  showMoreButton.id = 'show-more'
-  showMoreButton.innerText = 'Mostrar más'
-  document.body.append(showMoreButton)
-}
+  const showMoreButton = document.createElement('button');
+  showMoreButton.id = 'show-more';
+  showMoreButton.innerText = 'Mostrar más';
+
+  // Agregar evento de clic al botón
+  showMoreButton.addEventListener('click', () => {
+    // Aquí puedes poner la funcionalidad que deseas que ocurra cuando se haga clic en el botón
+    console.log('¡El botón "Mostrar más" ha sido clicado!');
+    // Por ejemplo, podrías llamar a una función o realizar alguna acción específica aquí
+  });
+
+  // Añadir el botón al cuerpo del documento
+  document.body.append(showMoreButton);
+};
