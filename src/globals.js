f0f84729f30215e85964215b502f9ebff37f1885
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-04-04 14:40:33 +0200

    Delete src/globals.js

--- src/globals.js
+++
@@ -1,6 +0,0 @@
-// Declara la variable currentPage como una variable global
-let currentPage = 1;
-
-// Exporta la variable currentPage para que pueda ser utilizada en otros archivos
-export { currentPage };
-export const searchInput = document.getElementById('searchBar');
