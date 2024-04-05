8e1918de877262c78d09b8a4edc594a96218c1ab
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-04-05 10:22:00 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -12,16 +12,3 @@ export function reloadPage() {
   window.location.reload(); // Recargar la página
 }
 
-/*
-// ESTO ES LO QUE PUSE YO PARA YO VER LOS FALLOS Y LO DE ARRIBA ES LA CORRECIÓN. //
-// Definir una función para restablecer la página
-export function resetPage() {
-  searchInput.value = ''; // Borrar la búsqueda
-  currentPage = 1; // Restablecer currentPage a 1
-  CallApi(''); // Llamar a la función de búsqueda con una cadena vacía para cargar imágenes automáticamente
-}
-
-// Definir una función para recargar la página
-export function reloadPage() {
-  window.location.reload(); // Recargar la página
-} */ 
