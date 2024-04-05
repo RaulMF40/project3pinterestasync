9363e769f21d42c6010d895643d61d81a1e4ef6c
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-04-05 10:25:25 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -12,3 +12,15 @@ export function reloadPage() {
   window.location.reload(); // Recargar la página
 }
 
+// esto de aqui abajo es lo que yo hice con fallos, y me lo han corregido que es lo de arriba 
+/* // Definir una función para restablecer la página
+export function resetPage() {
+  searchInput.value = ''; // Borrar la búsqueda
+  currentPage = 1; // Restablecer currentPage a 1
+  CallApi(''); // Llamar a la función de búsqueda con una cadena vacía para cargar imágenes automáticamente
+}
+
+// Definir una función para recargar la página
+export function reloadPage() {
+  window.location.reload(); // Recargar la página
+} */
